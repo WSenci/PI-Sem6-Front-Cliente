c18db4d0b484febcf1b9d58a51c3b8a9114d09ee
Wesley Senciani <wsa.wes05@gmail.com>
2025-03-19 21:22:20 -0300

    exclude backend files

--- connection/routes/product-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import ProductController from '../controllers/product-controller'
-
-const protuctRoutes = Router()
-
-protuctRoutes.get('/', ProductController.getProductList)
-
-export default protuctRoutes

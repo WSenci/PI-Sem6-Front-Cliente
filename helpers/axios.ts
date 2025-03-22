657bed69f7068637b39c642baff7ac1fc0099d93
WSenci <wsa.wes05@gmail.com>
2025-03-21 21:53:46 -0300

    crateing conection function with axios

---
+++ helpers/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Constants from "expo-constants";
+
+const uri = Constants.expoConfig?.hostUri ? `http://${Constants.expoConfig?.hostUri?.split(':').shift()}:3000` :  "";
+
+const api = axios.create({
+    baseURL: uri,
+    timeout: 2000
+});
+
+api.defaults.withCredentials = true;
+
+export default api;
